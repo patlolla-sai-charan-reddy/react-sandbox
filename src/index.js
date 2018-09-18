f3057bbbd2509a4810c9c273a8387f0cbc8489e6
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-09-18 15:28:18 -0400

    test

--- src/index.js
+++ src/index.js
@@ -5,14 +5,47 @@ import "./styles.css";
 
 console.log("React", React);
 
+/* functional Component - no data manipulation
+
+class based component - data and state management
+
+
+*/
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <li>hello</li>;
+}
+
+class Hey extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ""
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      data: "sai"
+    });
+  }
+  render() {
+    return (
+      <div>
+        <li>{this.state.data}</li>
+        <li>{this.props.data}</li>
+      </div>
+    );
+  }
+}
+
+class Apps extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <Hey data={"hello"} />;
+  }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Apps />, rootElement);
